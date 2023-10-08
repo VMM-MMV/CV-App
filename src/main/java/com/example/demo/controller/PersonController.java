2849363ab67d3e267d5499e7b0c1089c0445d362
miguel <mihai.vieru@isa.utm.md>
2023-10-08 16:45:41 +0300

    connects to react

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -6,8 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class PersonController {
+
     @Autowired
     PersonRepo repo;
 
