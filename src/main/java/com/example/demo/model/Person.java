f112305fa9c6c83ba59c731e336aa5ad1b25cfe7
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-10-12 10:56:31 +0300

    I set the size of the string for each field.

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -34,13 +34,12 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "person_id_seq")
     @SequenceGenerator(name = "person_id_seq", sequenceName = "person_id_seq", allocationSize = 1, initialValue = 1000)
     private long id;
-
+    @Column(length = 25)
     private String name;
-
+    @Column(length = 25)
     private String lastname;
-
     private LocalDate birthdate;
-
+    @Column(length = 60)
     private String city;
     @Enumerated(EnumType.STRING)
     private Sex sex;
@@ -48,45 +47,48 @@ public class Person {
     @Enumerated(EnumType.STRING)
     private Kids hasKids;
 
-    @Column(unique = true)
+    @Column(unique = true, length = 50)
     private String email;
-    @Column(table = "phone_number")
+    @Column(table = "phone_number", length = 5)
     private String countryCode;
-    @Column(table = "phone_number")
+    @Column(table = "phone_number", length = 15)
     private String phoneNumber;
+    @Column(length = 60)
     private String address;
+    @Column(length = 20)
     private String nationality;
     @Enumerated(EnumType.STRING)
+    @Column(length = 12)
     private CivilStatus civilStatus;
-    @Column(table = "education_info")
+    @Column(table = "education_info", length = 60)
     private String education;
-    @Column(table = "education_info")
+    @Column(table = "education_info", length = 60)
     private String school;
-    @Column(table = "education_info")
+    @Column(table = "education_info", length = 60)
     private String citySchool;
     @Column(table = "education_info")
     private LocalDate startDateStudy;
     @Column(table = "education_info")
     private LocalDate endDateStudy;
-    @Column(table = "job_history")
+    @Column(table = "job_history", length = 30)
     private String titleJob;
-    @Column(table = "job_history")
+    @Column(table = "job_history", length = 30)
     private String employer;
-    @Column(table = "job_history")
+    @Column(table = "job_history", length = 60)
     private String cityJob;
     @Column(table = "job_history")
     private LocalDate startDateJob;
     @Column(table = "job_history")
     private LocalDate endDateJob;
-    @Column(table = "job_history")
+    @Column(table = "job_history", columnDefinition = "text")
     private String descriptionJob;
-    @Column(table = "skills_info")
+    @Column(table = "skills_info", length = 50)
     private String skills;
-    @Column(table = "skills_info")
+    @Column(table = "skills_info", length = 50)
     private LevelSkills levelSkills;
-    @Column(table = "language_info")
+    @Column(table = "language_info", length = 50)
     private String language;
-    @Column(table = "language_info")
+    @Column(table = "language_info", length = 50)
     private LevelLanguage levelLanguage;
     private String hobby;
     private String achievements;
