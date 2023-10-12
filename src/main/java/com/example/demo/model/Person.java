b67ed907f3299705afff9f995039fe8ae78a7310
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-10-12 11:35:28 +0300

    I added getter and setter for all variables.

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -92,4 +92,244 @@ public class Person {
     private LevelLanguage levelLanguage;
     private String hobby;
     private String achievements;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    public Kids getHasKids() {
+        return hasKids;
+    }
+
+    public void setHasKids(Kids hasKids) {
+        this.hasKids = hasKids;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public CivilStatus getCivilStatus() {
+        return civilStatus;
+    }
+
+    public void setCivilStatus(CivilStatus civilStatus) {
+        this.civilStatus = civilStatus;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public void setEducation(String education) {
+        this.education = education;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public String getCitySchool() {
+        return citySchool;
+    }
+
+    public void setCitySchool(String citySchool) {
+        this.citySchool = citySchool;
+    }
+
+    public LocalDate getStartDateStudy() {
+        return startDateStudy;
+    }
+
+    public void setStartDateStudy(LocalDate startDateStudy) {
+        this.startDateStudy = startDateStudy;
+    }
+
+    public LocalDate getEndDateStudy() {
+        return endDateStudy;
+    }
+
+    public void setEndDateStudy(LocalDate endDateStudy) {
+        this.endDateStudy = endDateStudy;
+    }
+
+    public String getTitleJob() {
+        return titleJob;
+    }
+
+    public void setTitleJob(String titleJob) {
+        this.titleJob = titleJob;
+    }
+
+    public String getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(String employer) {
+        this.employer = employer;
+    }
+
+    public String getCityJob() {
+        return cityJob;
+    }
+
+    public void setCityJob(String cityJob) {
+        this.cityJob = cityJob;
+    }
+
+    public LocalDate getStartDateJob() {
+        return startDateJob;
+    }
+
+    public void setStartDateJob(LocalDate startDateJob) {
+        this.startDateJob = startDateJob;
+    }
+
+    public LocalDate getEndDateJob() {
+        return endDateJob;
+    }
+
+    public void setEndDateJob(LocalDate endDateJob) {
+        this.endDateJob = endDateJob;
+    }
+
+    public String getDescriptionJob() {
+        return descriptionJob;
+    }
+
+    public void setDescriptionJob(String descriptionJob) {
+        this.descriptionJob = descriptionJob;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public LevelSkills getLevelSkills() {
+        return levelSkills;
+    }
+
+    public void setLevelSkills(LevelSkills levelSkills) {
+        this.levelSkills = levelSkills;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public LevelLanguage getLevelLanguage() {
+        return levelLanguage;
+    }
+
+    public void setLevelLanguage(LevelLanguage levelLanguage) {
+        this.levelLanguage = levelLanguage;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public String getAchievements() {
+        return achievements;
+    }
+
+    public void setAchievements(String achievements) {
+        this.achievements = achievements;
+    }
 }
