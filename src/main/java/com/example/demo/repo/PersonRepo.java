b0992efe1b1ed63467f4958fcca50eb899f3e706
Ceban Vasile <vasile.ceban1@isa.utm.md>
2023-10-12 15:43:54 +0300

    I added findByEmail methods.

--- src/main/java/com/example/demo/repo/PersonRepo.java
+++ src/main/java/com/example/demo/repo/PersonRepo.java
@@ -4,8 +4,6 @@ import com.example.demo.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import java.util.List;
-
 @RepositoryRestResource
 public interface PersonRepo extends JpaRepository<Person, Long> {
     Person findByEmail(String email);
