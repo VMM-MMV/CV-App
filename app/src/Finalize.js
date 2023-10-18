3178fab84337ef40ef8e6f9781552b8d05ad24b9
Katrincik <grebennicova.ecaterina@gmail.com>
2023-10-18 16:04:30 +0300

    Add Finalize.js as last page

---
+++ app/src/Finalize.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Finalize() {
+  return (
+    <div id="parent">
+      <div className="aside-right">
+        <div className="container">
+          <div className="main-page">
+            <div className="space">
+              <h1 className="page-title">It's time to submit your CV!</h1>
+              <h2 className="sub-title">Your data has been successfully submitted.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Finalize;
