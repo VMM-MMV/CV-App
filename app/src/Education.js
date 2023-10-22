431ea5630d65fed353f1ad0c48c9cf7682ca7839
Katrincik <grebennicova.ecaterina@gmail.com>
2023-10-22 23:06:53 +0300

    Changed Education file similar to PersonForm from Child Hook to Child Class

--- app/src/Education.js
+++ app/src/Education.js
@@ -1,123 +1,132 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-function Education({ onDataCollected, data }) {
-    const [education, setEducation] = useState(data.education || '');
-    const [citySchool, setCitySchool] = useState(data.citySchool || '');
-    const [school, setSchool] = useState(data.school || '');
-    const [monthStart, setMonthStart] = useState(data.monthStart || '');
-    const [yearStart, setYearStart] = useState(data.yearStart || '');
-    const [monthEnd, setMonthEnd] = useState(data.monthEnd || '');
-    const [yearEnd, setYearEnd] = useState(data.yearEnd || '');
-
-    const months = Array.from({ length: 12 }, (_, i) => i + 1);
-    const currentYear = new Date().getFullYear();
-    const years = Array.from({ length: 100 }, (_, i) => currentYear - i);
-
-    const handleAddEducation = () => {
-        if (education && citySchool && school && monthStart && yearStart && monthEnd && yearEnd) {
-            const startDateStudy = `${yearStart}-${String(monthStart).padStart(2, '0')}`;
-            const endDateStudy = `${yearEnd}-${String(monthEnd).padStart(2, '0')}`;
-            const educationData = {
-                education,
-                citySchool,
-                school,
-                startDateStudy,
-                endDateStudy,
-            }
-            
-            onDataCollected('education', educationData);
-
-            setEducation('');
-            setCitySchool('');
-            setSchool('');
-            setMonthStart('');
-            setYearStart('');
-            setMonthEnd('');
-            setYearEnd('');
-        } else {
-            alert('Please fill out all fields.');
+class Education extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            startDateStudy: '',
+            endDateStudy: '',
+            monthStart: '',
+            monthEnd: '',
+            yearStart: '',
+            yearEnd: '',
         }
+        this.onDataCollected = props.onDataCollected;
+        this.data = props.data;
+    }
+    
+    handleMonthStartChange = (e) => {
+        this.setState({ monthStart: e.target.value });
+    }
+    
+    handleMonthEndChange = (e) => {
+        this.setState({ monthEnd: e.target.value });
+    }
+    
+    handleYearStartChange = (e) => {
+        this.setState({ yearStart: e.target.value });
     }
 
-    return(
-        <div id="parent">
-            <div className="aside-right">
-                <div className="container">
-                    <div className="main-page">
-                        <div className="space"> 
-                            <h1 className="page-title">Tell us about your education!</h1>
-                            <h2 className="sub-title">Type all necessary information about school, university where you studied. List them all.</h2>
-                            <div className="form2">
-                                <form onSubmit={handleAddEducation}>
-                                    <div className="degree-city-full-fields">
-                                        <div className="degree-field">
-                                            <label>
-                                                Degree:
-                                            </label>
-                                            <input type="text" required placeholder="e.g Master of Economics" className="form-name" autoComplete="given-educcation-degree" value={education} onChange={e => setEducation(e.target.value)}/>
-                                        </div>
-                                        <div className="city-field">
-                                            <label>
-                                                City/Town:
-                                            </label>
-                                            <input type="text" required placeholder="e.g Chisinau" className="form-name" autoComplete="given-city" value={citySchool} onChange={e => setCitySchool(e.target.value)}/>
-                                        </div>
-                                    </div>
-                                    <div className="school-full-fields">
-                                        <div className="school-field">
-                                            <label>
-                                                School:
-                                            </label>
-                                            <input type="text" required placeholder="e.g Universitatea Tehnica" className="form-name" autoComplete="given-school" value={school} onChange={e => setSchool(e.target.value)}/>
-                                        </div>
-                                    </div>
-                                    <div className="study-fields">
-                                        <div className="date-study-full-field">
-                                            <div className="date-study-field">
-                                                <div className="month-selector">
+    handleYearEndChange = (e) => {
+        this.setState({ yearEnd: e.target.value });
+    }
+
+    handleData() {
+        const { monthStart, monthEnd, yearStart, yearEnd } = this.state;
+        const startDateStudy = `${yearStart}-${String(monthStart).padStart(2, '0')}`;
+        const endDateStudy = `${yearEnd}-${String(monthEnd).padStart(2, '0')}`;
+        this.data.startDateStudy = startDateStudy; 
+        this.data.endDateStudy = endDateStudy; 
+
+        this.onDataCollected('education', this.data);
+
+    }
+
+    render () { 
+
+        const months = Array.from({ length: 12 }, (_, i) => i + 1);
+        const currentYear = new Date().getFullYear();
+        const years = Array.from({ length: 100 }, (_, i) => currentYear - i);
+
+        return(
+            <div id="parent">
+                <div className="aside-right">
+                    <div className="container">
+                        <div className="main-page">
+                            <div className="space"> 
+                                <h1 className="page-title">Tell us about your education!</h1>
+                                <h2 className="sub-title">Type all necessary information about school, university where you studied. List them all.</h2>
+                                <div className="form2">
+                                    <form>
+                                        <div className="degree-city-full-fields">
+                                            <div className="degree-field">
                                                 <label>
-                                                    Start Date:
+                                                    Degree:
                                                 </label>
-                                                    <select value={monthStart} className="form-name" onChange={e => setMonthStart(e.target.value)}>
-                                                        <option value="">Month</option>
-                                                        {months.map(m => <option key={m} value={m}>{m}</option>)}
-                                                    </select>
-                                                </div>
-                                                <div className="year-selector">
-                                                <label></label>
-                                                <select value={yearStart} className="form-name" onChange={e => setYearStart(e.target.value)}>
-                                                    <option value="">Year</option>
-                                                    {years.map(y => <option key={y} value={y}>{y}</option>)}
-                                                </select>
-                                                </div>  
-                                                <div className="month-selector">
+                                                <input type="text" required placeholder="e.g Master of Economics" className="form-name" autoComplete="given-educcation-degree" value={this.data.education} onChange={e => this.data.education = e.target.value}/>
+                                            </div>
+                                            <div className="city-field">
                                                 <label>
-                                                    End Date:
+                                                    City/Town:
                                                 </label>
-                                                    <select value={monthEnd} className="form-name" onChange={e => setMonthEnd(e.target.value)}>
-                                                        <option value="">Month</option>
-                                                        {months.map(m => <option key={m} value={m}>{m}</option>)}
-                                                    </select>
-                                                </div>
-                                                <div className="year-selector">
-                                                <label></label>
-                                                <select value={yearEnd} className="form-name" onChange={e => setYearEnd(e.target.value)}>
-                                                    <option value="">Year</option>
-                                                    {years.map(y => <option key={y} value={y}>{y}</option>)}
-                                                </select>
-                                                </div>  
+                                                <input type="text" required placeholder="e.g Chisinau" className="form-name" autoComplete="given-city" value={this.data.citySchool} onChange={e => this.data.citySchool = e.target.value}/>
                                             </div>
                                         </div>
-                                    </div>
-                                </form>
-                            </div>
-                         </div>
-                    </div>
-                </div>  
+                                        <div className="school-full-fields">
+                                            <div className="school-field">
+                                                <label>
+                                                    School:
+                                                </label>
+                                                <input type="text" required placeholder="e.g Universitatea Tehnica" className="form-name" autoComplete="given-school" value={this.data.school} onChange={e => this.data.school = e.target.value}/>
+                                            </div>
+                                        </div>
+                                        <div className="study-fields">
+                                            <div className="date-study-full-field">
+                                                <div className="date-study-field">
+                                                    <div className="month-selector">
+                                                    <label>
+                                                        Start Date:
+                                                    </label>
+                                                        <select value={this.state.monthStart} className="form-name" onChange={this.handleMonthStartChange}>
+                                                            <option value="">Month</option>
+                                                            {months.map(m => <option key={m} value={m}>{m}</option>)}
+                                                        </select>
+                                                    </div>
+                                                    <div className="year-selector">
+                                                    <label></label>
+                                                    <select value={this.state.yearStart} className="form-name" onChange={this.handleYearStartChange}>
+                                                        <option value="">Year</option>
+                                                        {years.map(y => <option key={y} value={y}>{y}</option>)}
+                                                    </select>
+                                                    </div>  
+                                                    <div className="month-selector">
+                                                    <label>
+                                                        End Date:
+                                                    </label>
+                                                        <select value={this.state.monthEnd} className="form-name" onChange={this.handleMonthEndChange}>
+                                                            <option value="">Month</option>
+                                                            {months.map(m => <option key={m} value={m}>{m}</option>)}
+                                                        </select>
+                                                    </div>
+                                                    <div className="year-selector">
+                                                    <label></label>
+                                                    <select value={this.state.yearEnd} className="form-name" onChange={this.handleYearEndChange}>
+                                                        <option value="">Year</option>
+                                                        {years.map(y => <option key={y} value={y}>{y}</option>)}
+                                                    </select>
+                                                    </div>  
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                             </div>
+                        </div>
+                    </div>  
+                </div>
             </div>
-        </div>
-    )
-
+        );
+    }    
 }
-
-export default Education
+    
+export default Education;
