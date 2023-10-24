daab8d73fa2a668ec031fb30492f97bdcf75f6e1
Katrincik <grebennicova.ecaterina@gmail.com>
2023-10-24 16:18:59 +0300

    Created the Initial page from where you could start to create your CV

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef } from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import Initial from './Initial'
 import PersonForm from './PersonForm';
 import Education from './Education';
 import Experience from './Experience';
@@ -12,8 +13,8 @@ import Finalize from './Finalize';
 import axios from 'axios';
 
 function App() {
-  const steps = ['person', 'education', 'experience', 'skills', 'languages', 'hobby', 'achievements', 'finalize'];
-  const [currentForm, setCurrentForm] = useState('person');
+  const steps = ['initial', 'person', 'education', 'experience', 'skills', 'languages', 'hobby', 'achievements', 'finalize'];
+  const [currentForm, setCurrentForm] = useState('initial');
 
   const [collectedData, setCollectedData] = useState({
     person: {},
@@ -26,6 +27,10 @@ function App() {
   });
 
   const childFormRef = useRef();
+
+  const handleStartClick = () => {
+    setCurrentForm('person');
+  };
 
   const handleNextClick = () => {
     // Try/Catch because not all forms may have 'handleData()' method.
@@ -105,6 +110,7 @@ function App() {
   return (
     <div className="App">
       <div className="opacity">
+        {currentForm === 'initial' && <Initial onStartClick={handleStartClick}/>}
         {currentForm === 'person' && <PersonForm ref={childFormRef} onDataCollected={onDataCollected} data={collectedData.person}/>}
         {currentForm === 'education' && <Education ref={childFormRef} onDataCollected={onDataCollected} data={collectedData.education}/>}
         {currentForm === 'experience' && <Experience ref={childFormRef} onDataCollected={onDataCollected} data={collectedData.experience}/>}
@@ -119,19 +125,21 @@ function App() {
               <div className="space">
                 <div className="form">
                   <div className={`button-submit ${currentForm === 'person' ? 'first-page' : ''}`}>
-                    {currentForm !== 'person' && (
+                    {steps.indexOf(currentForm) > steps.indexOf('person') && (
                         <button onClick={handleBackClick} className="button-field button-back" type="button">
                           Back
                         </button>
                     )}
-                    {isFinalizePage ? (
+                    {steps.indexOf(currentForm) >= steps.indexOf('person') && (
+                      isFinalizePage ? (
                         <button onClick={handleFinalSubmit} className="button-field button-next" type="button">
                           Submit
                         </button>
-                    ) : (
+                      ) : (
                         <button onClick={handleNextClick} className="button-field button-next" type="button">
                           Next page
                         </button>
+                      )
                     )}
                   </div>
                 </div>
