f49e5301bf3993d4efa16b06fcdb0f87e3d8d729
Vasile <vasile.ceban1@isa.utm.md>
2023-11-08 22:37:01 +0200

    I accessed Google Drive API and created the uploadResumeToDrive method.

--- src/main/java/com/example/demo/Gmail/GmailAPI.java
+++ src/main/java/com/example/demo/Gmail/GmailAPI.java
@@ -6,9 +6,13 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.ListMessagesResponse;
 import com.google.api.services.gmail.model.Message;
@@ -19,6 +23,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Paths;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
@@ -26,14 +31,18 @@ import java.util.Set;
 import static com.google.api.services.gmail.GmailScopes.GMAIL_READONLY;
 
 public class GmailAPI {
-    private final Gmail service;
+    private final Gmail serviceGmail;
+    private final Drive driveService;
 
     public GmailAPI() throws Exception {
         NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
         GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
 
-        service = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
+        serviceGmail = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
                 .setApplicationName("Test Gmail API")
+                .build();
+        driveService = new Drive.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
+                .setApplicationName("Test Google Drive API")
                 .build();
     }
     private Credential getCredentials(final NetHttpTransport http_Transport, GsonFactory jsonFactory)
@@ -41,7 +50,7 @@ public class GmailAPI {
 
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(Objects.requireNonNull(GmailAPI.class.getResourceAsStream("/credentials.json"))));
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                http_Transport, jsonFactory, clientSecrets, Set.of(GMAIL_READONLY))
+                http_Transport, jsonFactory, clientSecrets, Set.of(GMAIL_READONLY, DriveScopes.DRIVE_FILE))
                 .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
                 .setAccessType("offline")
                 .build();
@@ -52,7 +61,7 @@ public class GmailAPI {
     }
     public void listMessagesWithAttachments(String directory) {
         try {
-            ListMessagesResponse response = service.users().messages().list("me")
+            ListMessagesResponse response = serviceGmail.users().messages().list("me")
                     .setMaxResults(10L)
                     .execute();
 
@@ -60,7 +69,7 @@ public class GmailAPI {
 
             for (Message message : messages) {
                 String messageId = message.getId();
-                Message messageWithAttachments = service.users().messages().get("me", messageId)
+                Message messageWithAttachments = serviceGmail.users().messages().get("me", messageId)
                         .setFormat("full")
                         .execute();
 
@@ -85,7 +94,7 @@ public class GmailAPI {
     }
     public void getAttachments(String messageId, String attachmentId, String directory, String filename) {
         try {
-            MessagePartBody attachPart = service.users().messages().attachments().get("me", messageId, attachmentId).execute();
+            MessagePartBody attachPart = serviceGmail.users().messages().attachments().get("me", messageId, attachmentId).execute();
             byte[] fileByteArray = attachPart.decodeData();
 
             FileOutputStream fos = new FileOutputStream(directory + "/" + filename);
@@ -95,8 +104,34 @@ public class GmailAPI {
             e.printStackTrace();
         }
     }
+
+    public void uploadResumeToDrive(List<String> filePaths, String driveFolderId) {
+        try {
+            for (String filePath : filePaths) {
+                java.io.File fileContent = new java.io.File(filePath);
+                File fileMetadata = new File();
+                fileMetadata.setName(fileContent.getName());
+                fileMetadata.setMimeType("application/pdf");
+
+                File uploadedFile = driveService.files().create(fileMetadata, new FileContent("application/pdf", fileContent))
+                        .setFields("id")
+                        .execute();
+
+                if (driveFolderId != null && !driveFolderId.isEmpty()) {
+                    driveService.files().update(uploadedFile.getId(), null)
+                            .setFields("id, parents")
+                            .execute();
+                }
+
+                System.out.println("Fișierul " + fileContent.getName() + " a fost încărcat pe Google Drive.");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) throws Exception {
-            new GmailAPI().listMessagesWithAttachments( "C:\\Users\\Public\\Documents");
+            new GmailAPI().listMessagesWithAttachments( "C:\\Users\\Vasile\\cv");
+            new GmailAPI().uploadResumeToDrive(Collections.singletonList("C:\\Users\\Vasile\\cv\\MyCV.pdf"), "1KFTjVjo4qfR5-HsRQqKSTBk7RKyO5WKe");
     }
 
 }
