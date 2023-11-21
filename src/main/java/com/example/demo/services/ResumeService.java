f333855544ec01ecf37c7910c72788e1edf29b9c
Vasile <vasile.ceban1@isa.utm.md>
2023-11-21 23:01:34 +0200

    I added saving the date of sending the last message in lastMessageDate.txt

--- src/main/java/com/example/demo/services/ResumeService.java
+++ src/main/java/com/example/demo/services/ResumeService.java
@@ -21,6 +21,7 @@ import com.google.api.services.gmail.model.Message;
 import com.google.api.services.gmail.model.MessagePart;
 import com.google.api.services.gmail.model.MessagePartBody;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
@@ -28,69 +29,77 @@ import java.nio.file.Paths;
 import java.util.*;
 
 public class ResumeService {
-        private final Gmail serviceGmail;
-        private final Drive driveService;
+    private final Gmail serviceGmail;
+    private final Drive driveService;
 
-        public ResumeService() throws Exception {
-            NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-            GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
 
-            serviceGmail = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
-                    .setApplicationName("Service Gmail API")
-                    .build();
-            driveService = new Drive.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
-                    .setApplicationName("Service Google Drive API")
-                    .build();
-        }
+    public ResumeService() throws Exception {
+        NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
 
-        private Credential getCredentials(final NetHttpTransport http_Transport, GsonFactory jsonFactory)
-                throws IOException {
+        serviceGmail = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
+                .setApplicationName("Service Gmail API")
+                .build();
+        driveService = new Drive.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
+                .setApplicationName("Service Google Drive API")
+                .build();
+    }
 
-            GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(Objects.requireNonNull(ResumeService.class.getResourceAsStream("/credentials.json"))));
-            GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                    http_Transport, jsonFactory, clientSecrets, Set.of(GmailScopes.GMAIL_READONLY, DriveScopes.DRIVE_FILE))
-                    .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
-                    .setAccessType("offline")
-                    .build();
+    private Credential getCredentials(final NetHttpTransport http_Transport, GsonFactory jsonFactory)
+            throws IOException {
 
-            LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, new InputStreamReader(Objects.requireNonNull(ResumeService.class.getResourceAsStream("/credentials.json"))));
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                http_Transport, jsonFactory, clientSecrets, Set.of(GmailScopes.GMAIL_READONLY, DriveScopes.DRIVE_FILE))
+                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+                .setAccessType("offline")
+                .build();
 
-            return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-        }
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
 
-        public void listMessagesWithAttachments() {
-            try {
-                ListMessagesResponse response = serviceGmail.users().messages().list("me")
-                        .setMaxResults(10L)
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+    public void listMessagesWithAttachments() {
+        try {
+            ListMessagesResponse response = serviceGmail.users().messages().list("me")
+                    .setMaxResults(10L)
+                    .execute();
+
+            List<Message> messages = response.getMessages();
+            String lastMessageDate = null;
+            for (Message message : messages) {
+                String messageId = message.getId();
+                Message messageWithAttachments = serviceGmail.users().messages().get("me", messageId)
+                        .setFormat("full")
                         .execute();
 
-                List<Message> messages = response.getMessages();
+                String messageDate = String.valueOf(messageWithAttachments.getInternalDate());
 
-                for (Message message : messages) {
-                    String messageId = message.getId();
-                    Message messageWithAttachments = serviceGmail.users().messages().get("me", messageId)
-                            .setFormat("full")
-                            .execute();
+                if (lastMessageDate == null || messageDate.compareTo(lastMessageDate) > 0) {
+                    lastMessageDate = messageDate;
+                }
 
-                    MessagePart payload = messageWithAttachments.getPayload();
-                    if (payload != null) {
-                        List<MessagePart> parts = payload.getParts();
-                        if (parts != null) {
-                            for (MessagePart part : parts) {
-                                String filename = part.getFilename();
-                                if (filename != null && (filename.endsWith(".pdf") || filename.endsWith(".docx") || filename.endsWith(".doc"))) {
-                                    String attId = part.getBody().getAttachmentId();
-                                    uploadResumeToDrive(messageId, attId, "1KFTjVjo4qfR5-HsRQqKSTBk7RKyO5WKe", filename);
-                                }
+                MessagePart payload = messageWithAttachments.getPayload();
+                if (payload != null) {
+                    List<MessagePart> parts = payload.getParts();
+                    if (parts != null) {
+                        for (MessagePart part : parts) {
+                            String filename = part.getFilename();
+                            if (filename != null && (filename.endsWith(".pdf") || filename.endsWith(".docx") || filename.endsWith(".doc"))) {
+                                String attId = part.getBody().getAttachmentId();
+                                uploadResumeToDrive(messageId, attId, "1KFTjVjo4qfR5-HsRQqKSTBk7RKyO5WKe", filename);
                             }
                         }
                     }
                 }
-                System.out.println("CVs have been uploaded to the drive.");
-            } catch (IOException e) {
-                e.printStackTrace();
             }
+            writeDateToFile(lastMessageDate);
+            System.out.println("CVs have been uploaded to the drive.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
 
     public void uploadResumeToDrive(String messageId, String attachmentId, String folderId, String filename) {
         try {
@@ -122,48 +131,56 @@ public class ResumeService {
         driveService.files().create(fileMetadata, mediaContent).setSupportsTeamDrives(true).execute();
     }
 
-        public void deleteDuplicateFilesInDrive() {
-            try {
-                List<File> driveFiles = getDriveFiles("1KFTjVjo4qfR5-HsRQqKSTBk7RKyO5WKe");
-                Map<String, List<File>> fileMap = new HashMap<>();
+    public void deleteDuplicateFilesInDrive() {
+        try {
+            List<File> driveFiles = getDriveFiles("1KFTjVjo4qfR5-HsRQqKSTBk7RKyO5WKe");
+            Map<String, List<File>> fileMap = new HashMap<>();
 
-                for (File file : driveFiles) {
-                    String fileName = file.getName();
-                    fileMap.computeIfAbsent(fileName, k -> new ArrayList<>()).add(file);
-                }
+            for (File file : driveFiles) {
+                String fileName = file.getName();
+                fileMap.computeIfAbsent(fileName, k -> new ArrayList<>()).add(file);
+            }
 
-                for (Map.Entry<String, List<File>> entry : fileMap.entrySet()) {
-                    List<File> duplicates = entry.getValue();
-                    if (duplicates.size() > 1) {
-                        duplicates.sort(Comparator.comparingLong(file -> {
-                            DateTime modifiedTime = file.getModifiedTime();
-                            return (modifiedTime != null) ? modifiedTime.getValue() : 0L;
-                        }));
+            for (Map.Entry<String, List<File>> entry : fileMap.entrySet()) {
+                List<File> duplicates = entry.getValue();
+                if (duplicates.size() > 1) {
+                    duplicates.sort(Comparator.comparingLong(file -> {
+                        DateTime modifiedTime = file.getModifiedTime();
+                        return (modifiedTime != null) ? modifiedTime.getValue() : 0L;
+                    }));
 
-                        for (int i = 0; i < duplicates.size() - 1; i++) {
-                            File fileToDelete = duplicates.get(i);
-                            try {
-                                driveService.files().delete(fileToDelete.getId()).execute();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+                    for (int i = 0; i < duplicates.size() - 1; i++) {
+                        File fileToDelete = duplicates.get(i);
+                        try {
+                            driveService.files().delete(fileToDelete.getId()).execute();
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
                 }
-                System.out.println("Duplicate CVs were deleted.");
-            } catch (IOException e) {
-                e.printStackTrace();
             }
-        }
-
-        private List<File> getDriveFiles(String folderId) throws IOException {
-            String query = "'" + folderId + "' in parents and trashed=false";
-            return driveService.files().list().setQ(query).execute().getFiles();
-        }
-
-        public static void main(String[] args) throws Exception {
-            new ResumeService().listMessagesWithAttachments();
-            new ResumeService().deleteDuplicateFilesInDrive();
+            System.out.println("Duplicate CVs were deleted.");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
+
+    private List<File> getDriveFiles(String folderId) throws IOException {
+        String query = "'" + folderId + "' in parents and trashed=false";
+        return driveService.files().list().setQ(query).execute().getFiles();
+    }
+    private void writeDateToFile(String lasMessageDate) {
+        try {
+            FileWriter writer = new FileWriter("lastMessageDate.txt");
+            writer.write(lasMessageDate);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        new ResumeService().listMessagesWithAttachments();
+        new ResumeService().deleteDuplicateFilesInDrive();
+    }
+}
 
